fe73577c3fd4697382ba270dd4aa26dc9759d0eb
BuildTools <arobine.rbx@gmail.com>
2017-07-14 22:09:51 +0200

    battlefrance

---
+++ template1/index.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>WantonGraph</title>
+		<?php include("./forced.php"); ?>
+	</head>
+	<body id="body">
+		<?php include('./menu.php'); ?>
+	</body>
+</html>
---
+++ template1/menu.php
@@ -0,0 +1,5 @@
+<!-- DEBUT MENU -->
+
+
+
+<!-- FIN MENU -->
